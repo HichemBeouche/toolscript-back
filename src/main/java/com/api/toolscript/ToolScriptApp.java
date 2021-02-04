f2a70eba766b931ceb44b7d66e2692164b85cfe0
dengler5u <62470177+dengler5u@users.noreply.github.com>
2021-02-04 15:12:04 +0100

    Correction de l'authentification, gestion des tokens et inscription

---
+++ src/main/java/com/api/toolscript/ToolScriptApp.java
@@ -0,0 +1,13 @@
+package com.api.toolscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolScriptApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolScriptApp.class, args);
+	}
+
+}
